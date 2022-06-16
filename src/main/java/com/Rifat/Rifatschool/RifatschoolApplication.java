4b57e50202d1fad7ebc330df0f879c14eb3ceb24
Rifat Murtuza <rm.rudro@gmail.com>
2022-06-16 18:01:33 +0900

    first commit

---
+++ src/main/java/com/Rifat/Rifatschool/RifatschoolApplication.java
@@ -0,0 +1,13 @@
+package com.Rifat.Rifatschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifatschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RifatschoolApplication.class, args);
+	}
+
+}
